937977213356cbdd536bda342c9c68cf42d74b88
Peter <peter@afosto.com>
2017-04-20 15:32:40 +0200

    image is default added

--- src/Models/Products/Image.php
+++ src/Models/Products/Image.php
@@ -6,12 +6,14 @@ use Afosto\Data\Components\Model;
 
 /**
  * @property integer $id
+ * @property integer $is_default
  */
 class Image extends Model {
 
     public function getRules() {
         return [
             ['id', 'integer', true],
+            ['is_default', 'integer', true],
         ];
     }
 
